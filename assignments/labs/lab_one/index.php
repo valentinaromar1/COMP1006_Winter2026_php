845ad3ba5abc0011c6fff19c273b66ca774fa0c0
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2026-01-13 10:09:57 -0500

    update lab files

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,4 @@
 <?php 
-
-echo "<h1> Lab One - PHP Practice & Connecting to a DB </h1> ";
+require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
+require "footer.php"; 
