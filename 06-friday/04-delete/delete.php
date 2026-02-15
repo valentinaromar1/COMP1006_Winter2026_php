78e92fbbc52b928fad9dbba1c8cb267ac7d5e090
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-02-15 01:35:35 -0500

    made changes

--- 06-friday/04-delete/delete.php
+++ 06-friday/04-delete/delete.php
@@ -8,15 +8,15 @@
  * - Redirects back to orders.php
  */
 //connect to db
-
+require 'includes/connect.php'
 // make sure we received an ID
-
+$customerId = $_GET['id'];
 // create the query 
-
+$sql = "DELETE from orders1 where customer_id = :customer_id";
 //prepare 
-
+$stmt = $pdo->prepare($sql);
 //bind 
-
+$stmt->bindParam('customer_id',$customerId)
 //execute
 
 // Redirect back to admin list
