050113154bdb55732221441777c0ef5856bd04e8
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-04-10 16:04:48 -0400

    started on login

--- assignments/labs/course-project/login.php
+++ assignments/labs/course-project/login.php
@@ -1,7 +1,21 @@
 <?php 
+session_start();
 
 
+require "includes/connect.php";
 
 
+require "includes/header.php";
+
+$error = "";
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $usernameOrEmail = trim($_POST['username_or_email'] ?? '');
+    $password = $_POST['password'] ?? '';
+
+
+    
+}
 
 ?>
