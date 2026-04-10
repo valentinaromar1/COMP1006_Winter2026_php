a90185b054b8387d3e1e4e04d0663444ea9385d6
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-04-10 16:42:31 -0400

    made login

--- assignments/labs/course-project/login.php
+++ assignments/labs/course-project/login.php
@@ -15,7 +15,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
 
 
-    
+    if ($usernameOrEmail === '' || $password === '') {
+        $error = "Username/email and password are needed";
+    } 
+    else {
+        $sql = "SELECT id, username, email, password
+                FROM users
+                WHERE username = :login OR email = :login
+                LIMIT 1";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':login', $usernameOrEmail);
+
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+
+            session_regenerate_id(true);
+
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+
+            header("Location: index.php");
+            exit;
+        } 
+        else {
+            $error = "Invalid information, please try again";
+        }
+    }
 }
 
 ?>
